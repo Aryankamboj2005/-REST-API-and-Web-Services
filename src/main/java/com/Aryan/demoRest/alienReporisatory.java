57f3717d4c571fc440c17cd69109e221016976e4
Aryan <Aryankamboj2005@gmail.com>
2026-03-24 23:25:48 +0530

    adding or sending data from the client side simple

---
+++ src/main/java/com/Aryan/demoRest/alienReporisatory.java
@@ -0,0 +1,37 @@
+package com.Aryan.demoRest;
+import java.util.*;
+
+public class alienReporisatory {
+    private static List<alien> Aliens = new ArrayList<>();
+
+    static {
+        alien a1 = new alien();
+        a1.setMobile(90);
+        a1.setName("Aryan");
+        
+        alien a2 = new alien();
+        a2.setMobile(21);
+        a2.setName("Kamboj");
+        
+        Aliens.add(a1);
+        Aliens.add(a2);
+    }
+    
+    public List<alien> getAliens() {
+        return Aliens;
+    }
+    
+    public alien getAlien(int mobile) {
+    	for(alien a:Aliens) {
+    		if(a.getMobile()==mobile) {
+    			return a;
+    		}
+    	}
+    	
+    	return new alien();
+    }
+
+	public void create(alien a1) {
+		Aliens.add(a1);
+	}
+}
--- src/main/java/com/Aryan/demoRest/alienResources.java
+++ src/main/java/com/Aryan/demoRest/alienResources.java
@@ -1,31 +1,31 @@
 package com.Aryan.demoRest;
 
-import java.util.Arrays;
 import java.util.List;
-
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("alien")
 public class alienResources {
-
+      
+	public alienReporisatory repo = new alienReporisatory();
+	
 	@GET
 	@Produces(MediaType.APPLICATION_XHTML_XML)
-	public  List<alien> getAlien() {
-		alien a1 = new alien();
-		a1.setMobile(90);
-		a1.setName("Aryan");
-		alien a2 = new alien();
-		a2.setMobile(21);
-		a2.setName("Kamboj");
-		
-		
-		List<alien> aliens = Arrays.asList(a1,a2);
-		return aliens;
-				
-		
+	public List<alien> getAlien() {
+		return repo.getAliens();
+	}
+
+	@POST
+	@Path("create") 
+	@Consumes(MediaType.APPLICATION_XML) 
+	@Produces(MediaType.APPLICATION_XML) 
+	public alien createAlien(alien a1) {
+		repo.create(a1);
+		return a1;
 	}
 
 }
