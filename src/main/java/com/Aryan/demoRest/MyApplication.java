1f72f92030272f8b7314ba42e741f27e91e9498b
Aryan <Aryankamboj2005@gmail.com>
2026-03-25 20:48:03 +0530

    getting the specific data using the id number and getting data in the XML format

--- src/main/java/com/Aryan/demoRest/MyApplication.java
+++ src/main/java/com/Aryan/demoRest/MyApplication.java
@@ -1,13 +1,12 @@
 package com.Aryan.demoRest;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import jakarta.ws.rs.ApplicationPath;
 
-
+@ApplicationPath("/webapi")
 public class MyApplication extends ResourceConfig {
 	public MyApplication() {
 		// Scan the package for resource classes
 		packages("com.Aryan.demoRest");
-		register(alienResources.class);
-		register(MyResource.class);
 	}
 }
--- src/main/java/com/Aryan/demoRest/alienResources.java
+++ src/main/java/com/Aryan/demoRest/alienResources.java
@@ -1,6 +1,8 @@
 package com.Aryan.demoRest;
 
 import java.util.List;
+
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -14,8 +16,8 @@ public class alienResources {
 	public alienReporisatory repo = new alienReporisatory();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XHTML_XML)
-	public List<alien> getAlien() {
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public List<alien> getAliens() {
 		return repo.getAliens();
 	}
 
@@ -26,6 +28,13 @@ public class alienResources {
 	public alien createAlien(alien a1) {
 		repo.create(a1);
 		return a1;
+	}
+	@GET
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Path("singleAlien/{id}")
+	public alien getAlienById(@PathParam("id") int id){ //  means if the path have some id then the same 
+		               // id is used in the all the id variablke 
+		 return repo.getAlien(id);
 	}
 
 }
